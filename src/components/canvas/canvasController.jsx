5f1bd1f81cdb74e61b5c7d67d03540c564ec9a3f
amirreza_yar <yar.amirreza@gmail.com>
2025-08-08 22:56:05 +0330

    Adding left over commits before rebase

--- src/components/canvas/canvasController.jsx
+++ src/components/canvas/canvasController.jsx
@@ -23,6 +23,28 @@ import { useResizingContext } from '@/providers/hooks_provider/resizingProvider'
 import { useTapperingContext } from '@/providers/hooks_provider/tapperingProvider'
 import TapperingDrawer from './tappering/tapperingDrawer'
 
+function generateUUIDs(count) {
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const length = 6
+  const uuids = new Set()
+
+  while (uuids.size < count) {
+    let uuid = ''
+    for (let i = 0; i < length; i++) {
+      uuid += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+    uuids.add(uuid)
+  }
+
+  return Array.from(uuids)
+}
+
+function getDistance(nodeA, nodeB) {
+  const dx = nodeB?.left - nodeA?.left
+  const dy = nodeB?.top - nodeA?.top
+  return Math.round(Math.sqrt(dx * dx + dy * dy))
+}
+
 const CanvasControllers = ({ handleUndo, handleRedo }) => {
   const {
     topBarVisible,
@@ -45,18 +67,97 @@ const CanvasControllers = ({ handleUndo, handleRedo }) => {
   const { isResizingDrawerOpen } = useResizingContext()
   const { isTaperingDrawerOpen } = useTapperingContext()
 
-  const { canvasInstance } = useCanvasContext()
+  const {
+    canvasInstance,
+    setIsRulering,
+    setIsResizing,
+    setIsMoving,
+    setIsRemoving,
+    setIsDrawing,
+    setIsTappering,
+    setIsCrushFolding,
+    crushFoldObjectDirectionRef,
+  } = useCanvasContext()
+
+  const resetAll = () => {
+    setIsDrawing(false)
+    setIsRulering(false)
+    setIsResizing(false)
+    setIsMoving(false)
+    setIsRemoving(false)
+    setIsTappering(false)
+    setIsCrushFolding(false)
+  }
 
   const exportCanvasToJSON = () => {
     const canvas = canvasInstance.current
+    const flashing = {
+      nodes: [],
+      startCrushFold: false,
+      endCrushFold: false,
+      crushFoldDir: crushFoldObjectDirectionRef.current,
+    }
 
-    const objs = canvas
-      .getObjects()
-      .filter((obj) => obj.type === 'circle' || (obj.type === 'line' && obj.hitboxLine))
+    resetAll()
 
-    console.log(JSON.stringify(objs))
+    const circles = canvas.getObjects().filter((obj) => obj.type === 'circle' && !obj.mainCircle)
 
-    console.log(objs[0].hitboxLine)
+    const startCircle = circles.find((cir) => !cir.line1)
+
+    const uuids = generateUUIDs(circles.length)
+    let node = startCircle
+    let index = 0
+
+    // {
+    //   node_id: 'circle456',
+    //   next_node_id: 'circle789',
+    //   prev_node_id: 'circle123',
+    //   next_line_bside_length: 180,
+    //   left: 300,
+    //   top: 250,
+    // },
+
+    while (node) {
+      const nodeObject = {
+        node_id: uuids[index],
+        left: 0,
+        top: 0,
+      }
+
+      nodeObject.top = node.top
+      nodeObject.left = node.left
+
+      if (node.line1) {
+        nodeObject.prev_node_id = uuids[index - 1]
+      }
+
+      if (node.line2) {
+        nodeObject.next_node_id = uuids[index + 1]
+      }
+
+      if (
+        node.line2?.bSideLineLength &&
+        getDistance(node, node.line2?.circle2) !== Math.round(node.line2?.bSideLineLength)
+      ) {
+        nodeObject.next_line_bside_length = node.line2.bSideLineLength
+      }
+
+      if (!node.line1 && node.crushFoldObject) {
+        flashing.startCrushFold = true
+      }
+
+      if (!node.line2 && node.crushFoldObject) {
+        flashing.endCrushFold = true
+      }
+
+      flashing.nodes.push(nodeObject)
+
+      node = node.line2?.circle2
+
+      index += 1
+    }
+
+    console.log(flashing)
   }
 
   return (
