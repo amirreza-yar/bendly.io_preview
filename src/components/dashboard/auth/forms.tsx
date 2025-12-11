890038017493180faa6d2efe0068f251dac52bbd
amirreza_yar <yar.amirreza@gmail.com>
2025-12-11 13:59:05 +0330

    Login password validator bug fixed

--- src/components/dashboard/auth/forms.tsx
+++ src/components/dashboard/auth/forms.tsx
@@ -176,17 +176,7 @@ export const AuthEmailForm = ({
 };
 
 const LoginFormSchema = z.object({
-  password: z
-    .string("Please enter your password.")
-    .min(8, "Password must be at least 8 characters long.")
-    .max(64, "Password must be at most 64 characters long.")
-    .regex(/[A-Z]/, "Password must contain at least one uppercase letter.")
-    .regex(/[a-z]/, "Password must contain at least one lowercase letter.")
-    .regex(/[0-9]/, "Password must contain at least one number.")
-    .regex(
-      /[^a-zA-Z0-9]/,
-      "Password must contain at least one special character."
-    ),
+  password: z.string("Please enter your password."),
   email: z
     .string("Please enter your email address.")
     .trim()
